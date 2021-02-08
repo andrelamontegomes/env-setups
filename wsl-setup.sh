bd19746361c3cf1c3dc66990b0038dfb054bbb93
Andre Gomes <andre@gosmartsolar.com>
2021-02-08 13:16:38 -0600

    Add missing nodejs and yarn install commands

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -7,8 +7,10 @@ echo "Installing Dependencies"
 
 ## Install Node and Yarn
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo npm install --global yarn
 
 ## Install other dependencies
 sudo apt-get install expect curl git python libpq-dev zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs unzip zip whois bat ack-grep fd fzf silversearcher-ag
