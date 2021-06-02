dd8508d9aa1628a9a20f2330b161a957f3ab3261
Andre Gomes <andre@gosmartsolar.com>
2021-06-02 16:31:54 -0500

    Zap Update

--- linux-setup.sh
+++ linux-setup.sh
@@ -11,7 +11,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 ## Install other dependencies
-sudo apt-get install expect curl git python libpq-dev zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs unzip zip whois bat ack-grep fd fzf silversearcher-ag
+sudo apt-get install expect curl git python libpq-dev zlib1g-dev build-essential libssl-dev lib32readline8 libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs unzip zip whois bat ack-grep fd fzf silversearcher-ag
 
 ## Install snap for software applications download
 sudo apt install firefox i3 filezilla kleopatra
