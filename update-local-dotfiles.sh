850bb38faf0683dafd83f8fd3016a3f527437a20
andre <me@andrelgomes.com>
2022-02-02 18:49:26 -0600

    Sync: 2022-02-02 18:49:26

--- update-local-dotfiles.sh
+++ update-local-dotfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cp ./dotfiles/.bashrc ~
-cp ./dotfiles/.zshrc ~
-cp ./dotfiles/.vimrc ~
-cp ./dotfiles/.tmux.conf ~   
-cp ./dotfiles/.irbrc ~   
-cp ./dotfiles/.minttyrc ~   
-cp ./dotfiles/.gitconfig ~   
-cp ./dotfiles/.taskrc ~   
+cp ./dotfiles/.bashrc $HOME/.bashrc
+cp ./dotfiles/.zshrc $HOME/.zshrc
+cp ./dotfiles/.vimrc $HOME/.vimrc
+cp ./dotfiles/.tmux.conf $HOME/.tmux.conf   
+cp ./dotfiles/.irbrc $HOME/.irbrc   
+cp ./dotfiles/.minttyrc $HOME/.minttyrc   
+cp ./dotfiles/.gitconfig $HOME/.gitconfig   
+cp ./dotfiles/.taskrc $HOME/.taskrc   
